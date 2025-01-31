87e2256fca2f2e0508cf8b0e15cd7ae39dde0016
Nathan Ho <63426994+nkh361@users.noreply.github.com>
2025-01-31 14:30:28 -0600

    Add files via upload

---
+++ Rogue/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module edu.depaul.rogue {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens edu.depaul.rogue to javafx.fxml;
+    exports edu.depaul.rogue;
+}
